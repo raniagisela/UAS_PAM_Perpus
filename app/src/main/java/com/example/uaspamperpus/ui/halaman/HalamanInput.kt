0652b651ba391dbf29d8fadcfcf46bd9e1420983
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 21:45:18 +0700

    menambahkan column pada function EntrySewaBody

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanInput.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanInput.kt
@@ -1,17 +1,23 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspamperpus.R
 import com.example.uaspamperpus.model.DetailSewa
@@ -71,4 +77,23 @@ fun EntrySewaBody(
     onSewaValueChange: (DetailSewa) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
-){}
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        modifier = Modifier.padding(16.dp)
+    ){
+        FormInputSewa(
+            detailSewa = uiStateSewa.detailSewa,
+            onValueChange = onSewaValueChange,
+            modifier = modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSewa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(id = R.string.submit))
+        }
+    }
+}
