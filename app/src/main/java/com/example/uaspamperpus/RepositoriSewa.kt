5792e85aa9cb5d1756ad51bafbd06fbc6f888667
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 20:32:19 +0700

    Menambahkan suspend fun insertSewa, deleteSewa, dan updateSewa

--- app/src/main/java/com/example/uaspamperpus/RepositoriSewa.kt
+++ app/src/main/java/com/example/uaspamperpus/RepositoriSewa.kt
@@ -7,4 +7,8 @@ interface RepositoriSewa {
     fun getAllSewaStream(): Flow<List<Sewa>>
 
     fun getSewaStream(id: Int): Flow<Sewa?>
+
+    suspend fun insertSewa(sewa: Sewa)
+    suspend fun deleteSewa(sewa: Sewa)
+    suspend fun updateSewa(sewa: Sewa)
 }
