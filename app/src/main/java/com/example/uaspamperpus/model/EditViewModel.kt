aa8b4390fdd2eff2eca70036fc5fdeed8e8fe743
raniagisela <raniagslp@gmail.com>
2024-01-11 20:59:08 +0700

    menambahkan private variabel itemId

--- app/src/main/java/com/example/uaspamperpus/model/EditViewModel.kt
+++ app/src/main/java/com/example/uaspamperpus/model/EditViewModel.kt
@@ -1,2 +1,31 @@
 package com.example.uaspamperpus.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspamperpus.repositori.RepositoriSewa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSewa: RepositoriSewa
+): ViewModel() {
+    var sewaUiState by mutableStateOf(UIStateSewa())
+        private set
+
+    private val itemId : Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            sewaUiState = repositoriSewa.getSewaStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStateSewa(true)
+        }
+    }
+
