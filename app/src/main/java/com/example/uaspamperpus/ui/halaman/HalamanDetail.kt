a273ade2bb2762e261520b95d04345a04efce9f0
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 21:52:38 +0700

    menambahkan private fun ItemDetailsBody dan Column pada private fun ItemDetailsBody

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanDetail.kt
@@ -1,5 +1,8 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,17 +12,25 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspamperpus.R
 import com.example.uaspamperpus.model.DetailsViewModel
+import com.example.uaspamperpus.model.ItemDetailsUiState
 import com.example.uaspamperpus.model.PenyediaViewModel
+import com.example.uaspamperpus.model.toSewa
 import com.example.uaspamperpus.navigasi.DestinasiNavigasi
 import kotlinx.coroutines.launch
 
@@ -69,5 +80,38 @@ fun DetailsScreen(
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
         )
+    }
+}
+
+@Composable
+private fun ItemDetailsBody(
+    itemDetailsUiState: ItemDetailsUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier
+){
+    Column (
+        modifier = modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        ItemDetails(
+            sewa = itemDetailsUiState.detailSewa.toSewa(),modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(onClick = { deleteConfirmationRequired = true},
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired){
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = {deleteConfirmationRequired = false},
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
