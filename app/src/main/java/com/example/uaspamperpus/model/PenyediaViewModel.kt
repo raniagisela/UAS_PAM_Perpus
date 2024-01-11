164a065ae4d8e6305ba316fd5fbf891c033ae221
raniagisela <raniagslp@gmail.com>
2024-01-11 21:02:43 +0700

    menambahkan variabel di object PenyediaViewModel

--- app/src/main/java/com/example/uaspamperpus/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/uaspamperpus/model/PenyediaViewModel.kt
@@ -1,3 +1,31 @@
 package com.example.uaspamperpus.model
 
-object PenyediaViewModel {}
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            SewaViewModel(perpusApp().container.repositoriSewa)
+        }
+        initializer {
+            InputViewModel(perpusApp().container.repositoriSewa)
+        }
+        initializer {
+            DetailsViewModel(createSavedStateHandle(),
+                perpusApp().container.repositoriSewa
+            )
+        }
+        initializer {
+            EditViewModel(createSavedStateHandle(),
+                perpusApp().container.repositoriSewa
+            )
+        }
+    }
+}
+
+fun CreationExtras.perpusApp():PerpusApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as PerpusApp)
