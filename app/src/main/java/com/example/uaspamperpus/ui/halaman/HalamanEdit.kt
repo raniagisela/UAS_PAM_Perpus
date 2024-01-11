9cadede38a9a83c19c4bfa79e6d9d317fe330656
raniagisela <raniagslp@gmail.com>
2024-01-11 21:38:43 +0700

    menambahkan fun ItemEditScreen

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanEdit.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanEdit.kt
@@ -1,2 +1,26 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspamperpus.R
+import com.example.uaspamperpus.model.EditViewModel
+import com.example.uaspamperpus.model.PenyediaViewModel
+import com.example.uaspamperpus.navigasi.DestinasiNavigasi
+
+object ItemEditDestination : DestinasiNavigasi {
+    override val route= "item_edit"
+    override val titleRes= R.string.edit_sewa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
