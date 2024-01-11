e72fa46ee49fad1d4b4217b433976ee4886c8e1b
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 21:05:38 +0700

    menambahkan class DetailsViewModel

---
+++ app/src/main/java/com/example/uaspamperpus/model/DetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.uaspamperpus.model
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.uaspamperpus.repositori.RepositoriSewa
+
+class DetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSewa: RepositoriSewa
+) : ViewModel(){
+
+}
