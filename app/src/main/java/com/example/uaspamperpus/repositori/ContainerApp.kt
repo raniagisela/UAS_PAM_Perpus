50644f63bf2c00303f388beba16fe065d8ee3675
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 20:41:23 +0700

    Menambahkan class ContainerDataApp

--- app/src/main/java/com/example/uaspamperpus/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/uaspamperpus/repositori/ContainerApp.kt
@@ -1,5 +1,12 @@
 package com.example.uaspamperpus.repositori
 
+import android.content.Context
+
 interface ContainerApp{
     val repositoriSewa : RepositoriSewa
+}
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSewa: RepositoriSewa by lazy {
+        OfflineRepositoriSewa(DatabaseSewa.getDatabase(context).sewaDao())
+    }
 }
