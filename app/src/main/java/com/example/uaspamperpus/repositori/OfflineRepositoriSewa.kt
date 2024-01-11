96d688d307721d024692d1d53ce6bfc677d3261e
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 20:37:10 +0700

    Menambahkan override fun getAllSewaStream dan  getSewaStream

--- app/src/main/java/com/example/uaspamperpus/repositori/OfflineRepositoriSewa.kt
+++ app/src/main/java/com/example/uaspamperpus/repositori/OfflineRepositoriSewa.kt
@@ -1,7 +1,12 @@
 package com.example.uaspamperpus.repositori
 
+import com.example.uaspamperpus.data.Sewa
 import com.example.uaspamperpus.data.SewaDao
+import kotlinx.coroutines.flow.Flow
 
 class OfflineRepositoriSewa(private val sewaDao: SewaDao): RepositoriSewa {
+    override fun getAllSewaStream(): Flow<List<Sewa>> =sewaDao.getAllSewa()
+
+    override fun getSewaStream(id: Int): Flow<Sewa?> =sewaDao.getSewa(id)
 
 }
