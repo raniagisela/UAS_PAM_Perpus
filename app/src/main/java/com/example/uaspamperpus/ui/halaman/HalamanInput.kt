fe854632b82ac7ca785480551a82f2bd992a523a
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 21:39:10 +0700

    menambahkan object DestinasiInput

---
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanInput.kt
@@ -0,0 +1,10 @@
+package com.example.uaspamperpus.ui.halaman
+
+import com.example.uaspamperpus.R
+import com.example.uaspamperpus.navigasi.DestinasiNavigasi
+
+object DestinasiInput : DestinasiNavigasi{
+    override val route = "Input"
+    override val titleRes = R.string.input_data
+
+}
