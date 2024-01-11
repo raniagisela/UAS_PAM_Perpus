2822272090f9038e79361395efe8c7c986d29386
raniagisela <raniagslp@gmail.com>
2024-01-11 20:26:50 +0700

    menambahkan dao

--- app/src/main/java/com/example/uaspamperpus/data/SewaDao.kt
+++ app/src/main/java/com/example/uaspamperpus/data/SewaDao.kt
@@ -1,4 +1,25 @@
 package com.example.uaspamperpus.data
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
 interface SewaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Sewa)
+
+    @Update
+    suspend fun update(siswa: Sewa)
+
+    @Delete
+    suspend fun delete(siswa: Sewa)
+
+    @Query("SELECT * from tblSewa WHERE id = :id")
+    fun getSewa(id: Int): Flow<Sewa>
+
+    @Query("SELECT * from tblSewa ORDER BY nama ASC")
+    fun getAllSewa(): Flow<List<Sewa>>
 }
