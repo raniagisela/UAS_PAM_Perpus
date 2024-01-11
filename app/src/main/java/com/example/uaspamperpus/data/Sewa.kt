369118f544619d42c2b5806f2ae286cd09d23a89
raniagisela <raniagslp@gmail.com>
2024-01-11 20:24:13 +0700

    menambahkan variabel di data class Sewa

--- app/src/main/java/com/example/uaspamperpus/data/Sewa.kt
+++ app/src/main/java/com/example/uaspamperpus/data/Sewa.kt
@@ -1,8 +1,16 @@
 package com.example.uaspamperpus.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "tblSewa")
 data class Sewa(
-
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String,
+    val judul_buku: String,
+    val lama_meminjam: String,
+    val tanggal_kembali: String
 )
