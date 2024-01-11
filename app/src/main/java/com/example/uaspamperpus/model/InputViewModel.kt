92f40330a13e5818aee438ef9bcb185b0869fb8d
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 21:02:23 +0700

    menambahkan function DetailSewa.toSewa, Sewa.toUiStateSew, dan Sewa.toDetailSewa

--- app/src/main/java/com/example/uaspamperpus/model/InputViewModel.kt
+++ app/src/main/java/com/example/uaspamperpus/model/InputViewModel.kt
@@ -2,7 +2,9 @@ package com.example.uaspamperpus.model
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.uaspamperpus.data.Sewa
 import com.example.uaspamperpus.repositori.RepositoriSewa
 
 class InputViewModel(private val repositoriSewa: RepositoriSewa): ViewModel() {
@@ -39,4 +41,29 @@ data class DetailSewa (
     val judul_buku: String = "",
     val lama_meminjam: String = "",
     val tanggal_kembali: String = ""
+)
+
+fun DetailSewa.toSewa():Sewa = Sewa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+    judul_buku = judul_buku,
+    lama_meminjam = lama_meminjam,
+    tanggal_kembali = tanggal_kembali
+
+)
+fun Sewa.toUiStateSewa(isEntryValid: Boolean = false):UIStateSewa =UIStateSewa(
+    detailSewa = this.toDetailSewa(),
+    isEntryValid = isEntryValid
+)
+
+fun Sewa.toDetailSewa():DetailSewa = DetailSewa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+    judul_buku = judul_buku,
+    lama_meminjam = lama_meminjam,
+    tanggal_kembali = tanggal_kembali
 )
