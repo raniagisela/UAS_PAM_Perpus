f1705e3cb03ad6677dc7b76970bbd62ec33eb76d
raniagisela <raniagslp@gmail.com>
2024-01-11 21:13:21 +0700

    menambahkan override function di class PerpusApp

--- app/src/main/java/com/example/uaspamperpus/PerpusApp.kt
+++ app/src/main/java/com/example/uaspamperpus/PerpusApp.kt
@@ -1,4 +1,14 @@
 package com.example.uaspamperpus
 
-class PerpusApp {
+import android.app.Application
+import com.example.uaspamperpus.repositori.ContainerApp
+import com.example.uaspamperpus.repositori.ContainerDataApp
+
+class PerpusApp : Application(){
+    lateinit var container : ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
