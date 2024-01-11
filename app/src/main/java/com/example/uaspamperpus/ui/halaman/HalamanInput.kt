4c2936463a95010a04ef253c684de03663db913d
Anisafauziah1 <fzhnsa@gmail.com>
2024-01-11 21:40:18 +0700

    menambahkan function InputSewaScreen

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanInput.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanInput.kt
@@ -1,10 +1,20 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.uaspamperpus.R
 import com.example.uaspamperpus.navigasi.DestinasiNavigasi
 
 object DestinasiInput : DestinasiNavigasi{
     override val route = "Input"
     override val titleRes = R.string.input_data
-
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputSewaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InputViewModel = viewModel(factory = PenyediaViewModel.Factory )
+){}
