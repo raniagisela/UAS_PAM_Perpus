447a3cf044433760a33b54f2ea873d715c14b373
raniagisela <raniagslp@gmail.com>
2024-01-11 21:42:18 +0700

    menambahkan Scaffold

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanSewa.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanSewa.kt
@@ -1,8 +1,19 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspamperpus.R
 import com.example.uaspamperpus.model.PenyediaViewModel
@@ -22,4 +33,29 @@ fun SewaScreen(
     modifier: Modifier = Modifier,
     onDetailClick: (Int)->Unit={},
     viewModel: SewaViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SewaTopAppBar(
+                title = stringResource(DestinasiSewa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.input)
+                )
+            }
+        },
+    ) {}
