fbac3def3fb9ee1f1804705fc9bee458da5bfb38
raniagisela <raniagslp@gmail.com>
2024-01-11 21:36:10 +0700

    menambahkan Row

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/Halaman Home.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/Halaman Home.kt
@@ -1,10 +1,17 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -14,15 +21,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.uaspamperpus.R
 import com.example.uaspamperpus.navigasi.DestinasiNavigasi
 
@@ -98,3 +109,95 @@ fun CardElevation(judul: String, rating: String, desc:String, gambar:Int) {
             .padding(10.dp),
         shadowElevation = 10.dp
     ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Surface(
+                    shape = RoundedCornerShape(24.dp),
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color(0xFF070808)
+                ) {
+                    Text(
+                        text = "New release",
+                        fontSize =  12.sp,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = judul,
+                    fontSize =  24.sp,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = rating,
+                        fontSize =  14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = desc,
+                    fontSize =  11.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.size(width = 100.dp, height = 140.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = gambar),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
