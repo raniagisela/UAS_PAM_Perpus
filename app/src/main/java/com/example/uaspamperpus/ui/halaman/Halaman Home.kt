fc8c9cb76b5147899ad9ec9783073c6bceb33ca8
raniagisela <raniagslp@gmail.com>
2024-01-11 21:33:58 +0700

    menambahkan BodyHome

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/Halaman Home.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/Halaman Home.kt
@@ -1,6 +1,11 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -8,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
@@ -50,3 +56,22 @@ fun HomeScreen(
                 )
             }
         },
+    ) {
+            innerPadding ->
+        BodyHome(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth(),
+        )
+    }
+}
+
+@Composable
+fun BodyHome(
+    modifier: Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {}
