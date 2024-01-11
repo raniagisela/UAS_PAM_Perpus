32b5b656c29975e2f955cc515e38751e8aabcb8f
raniagisela <raniagslp@gmail.com>
2024-01-11 21:46:42 +0700

    menambahkan Card

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanSewa.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/HalamanSewa.kt
@@ -1,13 +1,19 @@
 package com.example.uaspamperpus.ui.halaman
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -16,6 +22,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -129,4 +137,42 @@ fun ListSewa(
 fun DataSewa(
     sewa: Sewa,
     modifier: Modifier = Modifier
-){}
+){
+
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = sewa.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = sewa.telpon,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+            Text(
+                text = "Alamat : "+sewa.alamat,
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Spacer(Modifier.weight(1f))
+            Text(
+                text = "Judul Buku : "+sewa.judul_buku,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+    }
+}
