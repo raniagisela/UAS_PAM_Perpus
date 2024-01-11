915272bb103ec0625efb599273918401d592a573
raniagisela <raniagslp@gmail.com>
2024-01-11 21:32:43 +0700

    menambahkan Scaffold

--- app/src/main/java/com/example/uaspamperpus/ui/halaman/Halaman Home.kt
+++ app/src/main/java/com/example/uaspamperpus/ui/halaman/Halaman Home.kt
@@ -1,8 +1,18 @@
 package com.example.uaspamperpus.ui.halaman
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.uaspamperpus.R
 import com.example.uaspamperpus.navigasi.DestinasiNavigasi
 
@@ -16,4 +26,27 @@ object DestinasiHome: DestinasiNavigasi {
 fun HomeScreen(
     navigateToRent: () -> Unit,
     modifier: Modifier = Modifier,
-){}
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SewaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToRent,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.note),
+                    contentDescription = stringResource(id = R.string.sewa)
+                )
+            }
+        },
